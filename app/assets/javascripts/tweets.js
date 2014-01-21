fe0f55ba47f80ec344778c4851bef2680726c888
Peter Day <peter.day4@gmail.com>
2014-01-20 20:28:39 -0800

    added httprequest and styling
    added httprequest and styling

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,7 +1,59 @@
-$.ajax({
-  url: "/api/retrieveTweets/abcd",
-  type: "GET",
-  success: function(response) {
-    debugger;
-  }
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status === 200) {
+    	var jsonResponse = JSON.parse(xhr.responseText);
+    	success(jsonResponse);
+    }
+}
+xhr.open('GET', '/api/retrieveTweets/abcd', true);
+xhr.send();
+
+
+
+function success(response){
+	for(i = 0; i < response.statuses.length; i++){
+    	
+    	$(".text").append("<div class='bubble'><h3>"+ response.statuses[i].retweet_count +" retweets!</h3><h2>"+response.statuses[i].user.screen_name+ 
+    		" ("+ response.statuses[i].user.followers_count +" followers)" + " said:</h2>" 
+    		+ response.statuses[i].text + "</div>");   
+    }
+}
+
+
+// $.ajax({
+//   url: "/api/retrieveTweets/abcd",
+//   type: "GET",
+//   success: function(response) {
+ 
+//     for(i = 0; i < response.statuses.length; i++){
+    	
+//     	$(".text").append("<div class='bubble'><h3>"+ response.statuses[i].retweet_count +" retweets!</h3><h2>"+response.statuses[i].user.screen_name+ 
+//     		" ("+ response.statuses[i].user.followers_count +" followers)" + " said:</h2>" 
+//     		+ response.statuses[i].text + "</div>");   	
+
+//     }
+
+   
+//    respondClick();
+//   }
+
+
+  
+// });
+
+function respondClick(){
+
+	$('.bubble').click(function(evt){
+		evt.stopPropagation();
+		
+		$(this).hide();
+
+	});
+}
+
+$(document).on("click", ".bubble", function(){
+	
+	$(this).hide();
 })
+
