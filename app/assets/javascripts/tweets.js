dddbc5f655a2684029bb50c0416c08387889a665
Peter Day <peter.day4@gmail.com>
2014-01-30 18:39:10 -0800

    added views for top 3 tweeters

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -8,10 +8,12 @@ function loadPosts(){
       }, 
         success: function(response) {
         
-        var template = window.JST["test"];
-        
-        $(".list").append(template(response));
+            var template = window.JST["test"];
+            
+            $(".list").append(template(response));
             listHover(response);
+            findLocation(response);
+            sortTweetCount(response);
         }
        
     });
@@ -26,18 +28,38 @@ loadPosts();
 
 function listHover (el){
     var template = window.JST["tweetCount"];
+    $('.popUp').hide();
     $(".bubble").hover(function(){
+        
+        var bubbleTop = $(this).offset().top + 50;
+        var bubbleIndex = $(this).index();
                 $('.popUp').show();
-                $('.popUp').append(template(el));
-               
+                $('.popUp').append(template({status: el.statuses[bubbleIndex]}));
+                $('.popUp').css("top",bubbleTop);
             
         }, function(){
-            $('.additionalInfo').find(".additionalInfo:last").remove();
+            $('.popUp').empty();
             $('.popUp').hide();
             
         }
     );
 }
+
+
+function findLocation(allTweets){
+    var locationCount = {};
+    
+    for(var i = 0; i < allTweets.statuses.length; i++ ){
+        var location = allTweets.statuses[i].user.location;
+        if(locationCount.hasOwnProperty(location)){
+            locationCount[location] += 1;
+        } else{
+            locationCount[location] = 1;
+        }
+    }
+    
+}
+
 
 $(document).ajaxComplete(function(){
     
@@ -61,12 +83,24 @@ $(document).scroll(function(){
     checkScroll();
 })
 
+//need to make this not affect order of other status_count
+function sortTweetCount(response){
+
+var sortedTweets = response.statuses.sort(function(a, b){
+    return b.user.statuses_count - a.user.statuses_count;
+    });
+    var topThree = sortedTweets.slice(0, 3);
+    
+    for(var i = 0; i < 3; i++){
+        $('.leftPanel').append("<h2>" + topThree[i].user.screen_name + ": " + topThree[i].user.statuses_count + "</h2>");
+    }
+}
 
 
-//top 3 tweeters, locations, more analysis
+
 
 var options = {
-    valueNames: ['retweet', 'name', 'text', 'tweetCount']
+    valueNames: ['retweet', 'name', 'text', 'tweetCount', 'followers']
 };
 
 
